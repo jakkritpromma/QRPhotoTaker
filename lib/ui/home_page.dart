0ff2135289b9cb98a42348c9e25679d2fdfd4c10
jakkritpromma <jakkrit.prommba.bid@gmail.com>
2025-01-01 17:45:52 +0700

    added SingleChildScrollView in home_page

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -11,19 +11,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
-    double buttonHeight = screenWidth / 2;
+    double buttonHeight = screenHeight * 0.2; // 20% of screen height
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Camera & QR Scanner'),
       ),
-      body: Center(
+      body: SingleChildScrollView(  // Ensure content can scroll if needed
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          padding: EdgeInsets.symmetric(horizontal: 40.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,  // Center vertically
+            crossAxisAlignment: CrossAxisAlignment.center,  // Center horizontally
             children: [
+              SizedBox(height: screenHeight * 0.15),  // Add space from the top (optional)
+
               // RaisedGradientButton 1
               RaisedGradientButton(
                 child: Text(
@@ -38,10 +42,11 @@ class HomePage extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => CameraPage()),
                 ),
                 borderRadius: 12.0,
-                width: screenWidth,
+                width: screenWidth * 0.8,  // Adjust width to prevent stretching
                 height: buttonHeight,
               ),
               SizedBox(height: 20),
+
               // RaisedGradientButton 2
               RaisedGradientButton(
                 child: Text(
@@ -56,9 +61,11 @@ class HomePage extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => QRScannerPage()),
                 ),
                 borderRadius: 12.0,
-                width: screenWidth,
+                width: screenWidth * 0.8,  // Adjust width to prevent stretching
                 height: buttonHeight,
               ),
+
+              SizedBox(height: screenHeight * 0.15),  // Add space from the bottom (optional)
             ],
           ),
         ),
